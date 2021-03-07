9c05453e9285494d20cb9098c5328fbd9488c901
qwerdh <1329788326@qq.com>
2021-03-07 19:38:17 +0800

    Delete JWXK-0.0.4 directory

--- JWXK-0.0.4/src/main/java/mapper/VJwGgxxXhMapper.java
+++
@@ -1,8 +0,0 @@
-package mapper;
-
-import java.util.List;
-
-public interface VJwGgxxXhMapper {
-	public List<String> findAllXh();
-	public List<Integer> findAllKcxh(String xh);
-}
--- JWXK-0.0.4/src/main/java/mapper/VJwLbdhMapper.java
+++
@@ -1,7 +0,0 @@
-package mapper;
-
-import java.util.List;
-
-public interface VJwLbdhMapper {
-   public List<String> findBefSelect(String xh);
-}
